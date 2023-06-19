4b5f19463db547844736e7c868917e7cb8930401
CharmaineM26 <Charmainem2023@gmail.com>
2023-06-19 10:23:35 -0700

    q7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+File Edit Options Buffers Tools Sh-Script Help                                                                                              
+#include <stdio.h>                                                                                                                          
+/**
+*main - main entry 
+*Description: Intel syntax 
+*Return: 1 (Success)
+*/
+
+int main(void)
+{
+    print f(" and that piece of art is useful" - Dora Korpar, 2015-10-19\n")                                                                                                                              
+    return (0)                                                                                                                              
+}
