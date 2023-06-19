37fd48203d2bf3acb030b27ea0a63a93a194361c
CharmaineM26 <Charmainem2023@gmail.com>
2023-06-19 12:00:25 -0700

    q8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-File Edit Options Buffers Tools Sh-Script Help                                                                                              
-#include <stdio.h>                                                                                                                          
+#include <stdio.h>
+#include <unistd.h>
 /**
-*main - main entry 
-*Description: Intel syntax 
+*main - main entry
+*Description: Intel syntax
 *Return: 1 (Success)
 */
-
 int main(void)
 {
-    print f(" and that piece of art is useful" - Dora Korpar, 2015-10-19\n")                                                                                                                              
-    return (0)                                                                                                                              
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
