6ba4bceba8f7a84d6385a5832d714c184b972802
CharmaineM26 <Charmainem2023@gmail.com>
2023-06-20 15:57:33 -0700

    q2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print alphabet 10 times in lowercase
+*Description: 'print alphabet x10 in lowercase'
+*Return: always 0
+*/
+
+void print_alphabet_x10(void)
+{
+
+char ch;
+int i;
+i=0;
+while (i<10)
+{
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
+
