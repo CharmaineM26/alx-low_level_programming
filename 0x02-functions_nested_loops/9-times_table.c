0265818889fe2041edcff60be3a00ea864a945dc
CharmaineM26 <Charmainem2023@gmail.com>
2023-06-21 11:13:14 -0700

    q9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,23 +9,37 @@
 void times_table(void)
 {
 
-	int i;
-	int j;
-	int k;
+	int x;
+	int y;
+	int z;
+	int u;
+	int d;
 
-for (i = 0 ; i <= 38 ; i++)
+for (x = 0 ; x <= 9 ; x++)
 {
-	for (j = 0 ; j <= 9 ; j++)
+	for (y = 0 ; y <= 9 ; y++)
 	{
-		k = i * j;
-		if (j != 0)
-			_putchar(',');
-		if (k < 10)
-			_putchar(' ');
-		else
-			_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
+		z = x * y;
+		if (z > 9)
+	{
+		u = z % 10;
+		d = (z - u) / 10;
+		_putchar(44);
+		_putchar(32);
+		_putchar(d + '0');
+		_putchar(u + '0');
 	}
+	else
+	{
+		if (y != 0)
+		{
+			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+		}
+		_putchar(z + '0');
+	}
+}
 	_putchar('\n');
 }
 }
