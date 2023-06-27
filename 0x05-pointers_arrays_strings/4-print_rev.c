c4d5b410066fe49f949ea4ebd588e3c5f639a5b0
Charmaine Motlhabi <charmainem2023@gmail.com>
2023-06-27 16:02:08 +0200

    q4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,19 @@
 #include "main.h"
+#include <unistd.h>
+#include <string.h>
 
 /**
 *print_rev - prints string in reverse
 *@s: the input pointer
-*Return: string in reverse
+*Return: void
 */
 
 void print_rev(char *s)
 {
-if (s == NULL)
-{
-return;
-}
-int length; 
-length = 0;
-
-while (s[length] != '\0')
-{
-length++;
-}
+int len = strlen(s);
 int i;
-for (i = length - 1 ; i >= 0 ; i--)
+ 
+for (i = len - 1 ; i >= 0 ; i--)
 {
 putchar(s[i]);
 }
