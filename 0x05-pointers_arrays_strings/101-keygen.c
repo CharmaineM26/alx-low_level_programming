9038d4b3b2917a02de707095bb5eeb24d524991f
Charmaine Motlhabi <charmainem2023@gmail.com>
2023-06-28 13:48:07 +0200

    q11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,34 @@
-/**
- *_atoi - converts a string to an integer
- *@
- *Return: no numbers in string 0 (success)
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main(void)
+#define PASSWORD_LENGTH 8
+
+char generateRandomNumber()
 {
+return 'A' + rand() % 26; 
+}
 
+void generateRandomPassword(char *password)
+{
+int i;
+
+for (i = 0 ; i <= PASSWORD_LENGTH ; i++)
+{
+password[i] = generateRandomNumber();
+}
+password[PASSWORD_LENGTH] = '\0';
+}
+
+int main()
+{
+srand (time(NULL));
+
+char password[PASSWORD_LENGTH + 1];
+
+generateRandomPassword(password);
+
+printf("Generated Password: %s\n", password);
+
+return (0);
 }
