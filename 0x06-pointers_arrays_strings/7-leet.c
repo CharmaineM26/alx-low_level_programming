40eff13b1a3c5879d90fb63ec437f80b776e68b1
Charmaine Motlhabi <charmainem2023@gmail.com>
2023-06-30 22:09:10 +0200

    q7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*leet - encodes a string into 1337
+*@n: input string pointer to be modified
+*Return: a pointer to a modified character
+*/
+
+char *leet(char *n)
+{
+
+  char str1[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+ char str2[] = {52, 51, 48, 55, 49, 53};
+
+int i, j;
+
+for (i = 0 ; n[i] != '\0' ; i++)
+{
+for (j = 0 ; j < 10 ; j++)
+{
+if (n[i] == str1[j])
+{
+n[i] = str2[j];
+}
+}
+}
+return (n);
+}
