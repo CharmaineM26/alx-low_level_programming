1762f2c23031b871fdd97d9626302b734bdd2a36
Charmaine Motlhabi <charmainem2023@gmail.com>
2023-07-10 16:26:24 +0200

    q1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - print the numbers if argumenrs passed to the program
+*@argv: array of arguments
+*@argc: argument count
+*Return: 0 always (success)
+*/
+
+int main(int argc, char *argv[])
+
+{
+(void)argv;
+printf("%d\n", argc-1);
+
+return (0);
+}
