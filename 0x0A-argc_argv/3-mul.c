0fb3ff5d661de011a462f2d4ac12607bce6717c2
Charmaine Motlhabi <charmainem2023@gmail.com>
2023-07-11 11:10:41 +0200

    q3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_atoi - converts a string to an integer
+*@s: the string to convert
+*Return: the converted integer
+*/
+
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+int i = 0;
+
+if (s[0] == '-')
+{
+sign = -1;
+i++;
+}
+
+for (; s[i] != '\0'; i++)
+{
+result = result * 10 + (s[i] - '0');
+}
+return (sign *result);
+}
+
+/**
+* main - program that multiplies two numbers
+*@argv: argument array
+*@argc: argument count
+*Return: 0 (success) else 1
+*/
+
+int main(int argc, char *argv[])
+
+{
+
+int num1, num2, result;
+
+if (argc != 3)
+
+{
+printf("ERROR\n");
+return (1);
+}
+
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+
+result = num1 *num2;
+
+{
+printf("%d\n", result);
+}
+return (0);
+}
