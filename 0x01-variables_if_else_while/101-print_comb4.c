c5732b40485af0866eaba012762e8232771ae9a8
CharmaineM26 <charmainem2023@gmail.com>
2023-07-26 14:05:16 +0200

    q12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,15 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * main - all possible different cobinations of three digits
+ *Return: always 0 (success)
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
         int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
+        
         return (0);
 }
