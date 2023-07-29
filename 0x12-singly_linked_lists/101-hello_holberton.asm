a5ffc791fafc819be62bc4275d346bdaf7c6efea
CharmaineM26 <charmainem2023@gmail.com>
2023-07-29 02:55:15 +0200

    q6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,14 @@
+section .data
+format db 'Hello, Holberton', 0
+
+section .text
 global main
 extern printf
-main:
-mov edi, format
-xor eax, eax
-call printf
-mov eax, 0
-ret
-format:	db 'Hello, Holberton\n',0
 
+main:
+mov edi, format 	; Move the address of the format string into EDI register
+xor eax, eax	 ;Clear EAX register (necessary for some calling conventions)
+call printf		 ;Call the printf function
+mov eax, 0		;Set the return value of the main function to 0 (success)
+ret	 ;Return from the main function
 
