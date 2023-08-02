0ae59df23beb42e6df7fabcaa0b5843209d455be
CharmaineM26 <charmainem2023@gmail.com>
2023-08-02 04:14:00 +0200

    q0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,15 +5,40 @@
 *@h: linked list of type listint_t to print
 *Return: number of nodes
 */
-
 size_t print_listint(const listint_t *h)
 {
-
 size_t num = 0;
 
 while (h)
 {
-printf("%d\n", h->n);
+int num_digits = 0;
+int temp = h->n;
+
+if (temp == 0)
+{
+num_digits = 1;
+}
+else
+while (temp != 0)
+{
+num_digits++;
+temp /= 10;
+}
+}
+
+if (h->n < 0)
+
+{
+putchar('_');
+num_digits--;
+h->n = -(h->n);
+}
+for (int i = num_digits - 1; i >= 0; i--)
+{
+int digit = (h->n / pow(10, i)) % 10;
+putchar(digit + '0');
+}
+putchar('\n');
 num++;
 h = h->next;
 }
