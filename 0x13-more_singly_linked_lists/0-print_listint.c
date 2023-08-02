6fded28d17f971f28a726e98c8634b0de98c410e
CharmaineM26 <charmainem2023@gmail.com>
2023-08-02 04:30:49 +0200

    q0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,34 +11,7 @@ size_t num = 0;
 
 while (h)
 {
-int num_digits = 0;
-int temp = h->n;
-
-if (temp == 0)
-{
-num_digits = 1;
-}
-else
-while (temp != 0)
-{
-num_digits++;
-temp /= 10;
-}
-}
-
-if (h->n < 0)
-
-{
-putchar('_');
-num_digits--;
-h->n = -(h->n);
-}
-for (int i = num_digits - 1; i >= 0; i--)
-{
-int digit = (h->n / pow(10, i)) % 10;
-putchar(digit + '0');
-}
-putchar('\n');
+printf("%d\n", h->n);
 num++;
 h = h->next;
 }
