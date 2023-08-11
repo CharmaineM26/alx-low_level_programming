822e9700c69ec3c363b93b3a9a0a1d3d071b36a6
CharmaineM26 <charmainem2023@gmail.com>
2023-08-11 12:46:31 +0200

    q4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,11 +5,51 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+void print_usage_and_exit(void);
+void error_exit(const char *message, const char *filename, int code);
+int open_source_file(const char *file_from);
+int open_destination_file(const char *file_to, mode_t mode);
+void copy_file(int fd_from, int fd_to);
+ssize_t read_and_write(int fd_from, int fd_to);
+
 /**
-*print_usage_and_exit -if the file is not an ELF file - exit code 98
-*Return: void
+*main - entry point to display contents of ELF file
+*@argc: argument count
+*@argv: argument varibale
+*Return: contents of the ELF FILE else 98 code
 */
 
+int main(int argc, char *argv[])
+{
+char *file_from;
+char *file_to;
+int fd_from;
+int fd_to;
+
+if (argc != 3)
+{
+print_usage_and_exit();
+}
+
+file_from = argv[1];
+file_to = argv [2];
+ 
+fd_from = open_source_file(file_from);
+fd_to = open_destination_file(file_to, S_IRUSR | S_IWUSR | S_IRGRP
+| S_IROTH);
+copy_file(fd_from, fd_to);
+
+close(fd_from);
+close(fd_to);
+
+return (0);
+}
+
+
+/**
+*print_usage_and_exit - if the file is not an ELF file - exit code 97
+*Return: void
+*/
 void print_usage_and_exit(void)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -17,11 +57,13 @@ exit(97);
 }
 
 /**
- *error_exit - the error to be generated if not ELF or error
- *@message: error message to be printed
- *@filename: file name of that generates error
- *@code: status code 98
+*error_exit - the error to be generated if not ELF or error
+*@message: error message to be printed
+*@filename: file name of that generates error
+*@code: status code 98
+*Return: returns file descriptor
 */
+
 void error_exit(const char *message, const char *filename, int code)
 {
 dprintf(STDERR_FILENO, message, filename);
@@ -30,82 +72,76 @@ exit(code);
 }
 
 /**
- *copy_file - function that copies file contents to another file
- *@file_from: file to be copies from
- *@file_to: file to be copies to
+*open_source_file - opens sources file specified by file_from
+*@file_from: the specified file
+*Return: returns file descriptor
 */
-
-void copy_file(const char *file_from, const char *file_to)
-
-int fd_from;
-int fd_to;
-
+int open_source_file(const char *file_from)
 {
-int fd_from = open(file_from, O_RDONLY);
+int fd_from;
+
+fd_from = open(file_from, O_RDONLY);
 if (fd_from == -1)
 {
-error_exit("Error: Can't read from file %s", file_from, 98);
+error_exit("Error: Can'tread from file %s", file_from, 98);
+}
+return (fd_from);
 }
 
-int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+/**
+*open_destination_file - opens destination file
+*@file_to: destination file
+*@mode: file permissions mode
+*Return: file descriptor
+*/
+int open_destination_file(const char *file_to, mode_t mode)
+{
+int fd_to;
+fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, mode);
 if (fd_to == -1)
 {
-close(fd_from);
 error_exit("Error: Can't write to %s", file_to, 99);
 }
+return (fd_to);
+}
 
-char buffer[1024];
+
+/**
+*copy_file - function that copies file contents to another file
+*@fd_from: file to be copies from
+*@fd_to: file to be copies to
+*Returns: file descriptor
+*/
+void copy_file(int fd_from, int fd_to)
+{
 ssize_t bytes_read;
-ssize_t bytes_written;
-int fd_from;
-int fd_to;
+while ((bytes_read = read_and_write(fd_from, fd_to)) > 0)
+{
 
-
-while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read)
-{
-close(fd_from);
-close(fd_to);
-error_exit("Error: Can't write to %s", file_to, 99);
-}
-}
-
-if (bytes_read == -1)
-{
-close(fd_from);
-close(fd_to);
-error_exit("Error: Can't read from file %s", file_from, 98);
-}
- 
-if (close(fd_from) == -1 || close(fd_to) == -1)
-{
-error_exit("Error: Can't close fd", "", 100);
 }
 }
 
 /**
- *main - entry point to display contents of ELF file
- *@argc: argument count
- *@argv: argument varibale
- *Return: coontents of the ELF FILE else 98 code
+*read_and_write - reads data from source and copies to destination
+*@fd_from: file data is read from
+*@fd_to: file data to copied to
+*Return: file descriptor
 */
-
-int main(int argc, char *argv[])
+ssize_t read_and_write(int fd_from, int fd_to)
 {
-if(argc != 3)
+char buffer[1024];
+ssize_t bytes_written;
+ 
+ssize_t bytes_read = read(fd_from, buffer, sizeof(buffer));
+if (bytes_read == -1)
 {
-print_usage_and_exit();
+error_exit("Error: can't read from source file", "", 98);
+}
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written != bytes_read)
+{
+error_exit("Error: Can't write to destination file", "", 99);
 }
 
-char *file_from;
-char *file_to;
-
-file_from = argv[1];
-file_to = argv[2];
-
-copy_file(file_from, file_to);
-
-return (0);
+return (bytes_read);
 }
